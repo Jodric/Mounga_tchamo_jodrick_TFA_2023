8b01390d4f6817894551fa11f6d6df65b275c62a
Jodric <tchamojodrick@gmail.com>
2023-05-04 02:25:15 +0200

    debut style

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
-console.info('Hello world');
+"use strict";
+
+// burger menu
+
+const menuButton = document.querySelector('.nav__button');
+const menu = document.querySelector('.menu');
+
+
+menuButton.addEventListener('click', function () {
+  menu.classList.toggle('show__menu');
+  menuButton.classList.toggle('close');
+});
+
+const menu__el = document.querySelectorAll('.menu__el');
+
+menu__el.forEach(part => {
+  part.addEventListener('click', (e) => {
+    menu.classList.toggle('show__menu');
+    menuButton.classList.toggle('close');
+  });
+});
